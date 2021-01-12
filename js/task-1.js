935db6d8f6b0909dd83738821a0bcd4b8572f977
Vladislav Churilov <88vvch88@gmail.com>
2021-01-12 21:43:10 +0200

    дз 6

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+
+// import Users from './js/users.js';
+import users from './users.js';
+const getUserNames = users => users.map (function (user) {  
+    return user.name;
+  });
+
+  console.log(getUserNames(users));
+  // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 
+//   'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
