934f2ff961539e67a181a95c513b4aa42bc1845c
Vladislav Churilov <88vvch88@gmail.com>
2021-01-16 12:09:12 +0200

    дз 6

--- js/task-10.js
+++ js/task-10.js
@@ -1,11 +1,24 @@
 import users from './users.js';
-const getSortedUniqueSkills = users =>Object.values(users).sort(function(prevSkills, nextSkills) {
-    // console.log(user.skills);
-    return prevSkills.skills - nextSkills.skills;
+const getSortedUniqueSkills = users  =>users.map(function(user) {
+  console.log(...user.skills);
+  // if (user.skills.includes(skillsName)){        
+  //   skills.push(...user.skills);
+    return ;
+  
+  // return skills;
+  // console.log(user.skills);
+  // console.log(user.skills.hasOwnProperty(index)? false:(user.skills[index] = true));
+    // console.log(user.skills[index]);
+    // console.log(user.skills.indexOf(index));
+    // user.skills.indexOf(...user.skills) === index
+    
   });
-//   .sort(function(prevSkills, nextSkills){
-//     return prevSkills.skills - nextSkills.skills;
-//   });
+  // .filter (function(user){
+  // return ;
+  // })
+  // .sort(function(prevSkills, nextSkills){
+  //   return prevSkills.skills - nextSkills.skills;
+  // });
   
   console.log(getSortedUniqueSkills(users));
   // [ 'adipisicing', 'amet', 'anim', 'commodo', 
