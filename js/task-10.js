3d701ea03fe6247538a6c01b75c013354d101687
Vladislav Churilov <88vvch88@gmail.com>
2021-01-17 00:31:32 +0200

    дз 6

--- js/task-10.js
+++ js/task-10.js
@@ -1,30 +1,14 @@
 import users from './users.js';
-const getSortedUniqueSkills = users  =>users.map(function(user) {
-  console.log(...user.skills);
-  // if (user.skills.includes(skillsName)){        
-  //   skills.push(...user.skills);
-    return ;
-  
-  // return skills;
-  // console.log(user.skills);
-  // console.log(user.skills.hasOwnProperty(index)? false:(user.skills[index] = true));
-    // console.log(user.skills[index]);
-    // console.log(user.skills.indexOf(index));
-    // user.skills.indexOf(...user.skills) === index
-    
-  });
-  // .filter (function(user){
-  // return ;
-  // })
-  // .sort(function(prevSkills, nextSkills){
-  //   return prevSkills.skills - nextSkills.skills;
-  // });
-  
+const getSortedUniqueSkills = users => users.reduce(function(allSkills, user) {  
+ allSkills.push(...user.skills)
+  return allSkills ;
+}, [])
+.filter(function(allSkills,index,skillsArray ){ 
+  return skillsArray.indexOf(allSkills) === index;
+})
+.sort();
   console.log(getSortedUniqueSkills(users));
   // [ 'adipisicing', 'amet', 'anim', 'commodo', 
 //   'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 
 //   'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident',
 //    'tempor', 'velit', 'veniam' ]
-// Object.values(users).filter(function(user) {
-//     // console.log(user.skills);
-//     return user.skills;
